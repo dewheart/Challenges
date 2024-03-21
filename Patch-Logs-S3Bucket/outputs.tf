9325785d426064be6f69bcc8a231713c455e19be
Adewunmi Ibironke <dewheart@outlook.com>
2024-03-21 17:37:05 -0400

    Updating the path S3 bucket files

--- Patch-Logs-S3Bucket/outputs.tf
+++ Patch-Logs-S3Bucket/outputs.tf
@@ -2,3 +2,15 @@ output "patchS3bucket_arn" {
   description = "This is the public ip of the new web server"
   value       = aws_s3_bucket.patchS3bucket.arn
 }
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
