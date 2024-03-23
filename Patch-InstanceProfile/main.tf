8c52aac05c257fa0e538887556fdc4ee5d6bdafc
Adewunmi Ibironke <dewheart@outlook.com>
2024-03-22 20:10:14 -0400

    instance profile script

--- Patch-InstanceProfile/main.tf
+++ Patch-InstanceProfile/main.tf
@@ -5,17 +5,24 @@ locals {
 
 resource "aws_iam_instance_profile" "ssmpatch_profile" {
   name = "${local.default_tag}-ssmpatchprofile"
+  #  path = "/"
   role = aws_iam_role.ssmpatch_role.name
-  tags = {
-#    QSConfigId-quick-setup-configuration-id = quick-setup-configuration-id
-    Environment = var.EnvironmentName
-  }
+  #  tags = {
+  #    QSConfigId-quick-setup-configuration-id = quick-setup-configuration-id
+  #    Environment = var.EnvironmentName
+  #  }
 }
 
-resource "aws_iam_role_policy_attachment" "ssmpolicy_attachment" {
+resource "aws_iam_role_policy_attachment" "ssmpolicy_attachment1" {
   role       = aws_iam_role.ssmpatch_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+resource "aws_iam_role_policy_attachment" "ssmpolicy_attachment2" {
+  role       = aws_iam_role.ssmpatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMMaintenanceWindowRole"
+}
+
 
 data "aws_iam_policy_document" "ssmpatchiam_policy" {
   statement {
@@ -23,23 +30,22 @@ data "aws_iam_policy_document" "ssmpatchiam_policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["ssm.amazonaws.com"]
+      identifiers = ["ec2.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
   }
+}
 
+data "aws_iam_policy_document" "ssmpatchiam_policy2" {
   statement {
-#    principals {
-#      type        = "AWS"
-#      identifiers = "${local.account_id}"
-#    }
+    effect = "Allow"
 
     actions = [
       "s3:GetObject",
       "s3:PutObject",
-      "s3:PutObjectAcl",   
-      "s3:GetEncryptionConfiguration",   
+      "s3:PutObjectAcl",
+      "s3:GetEncryptionConfiguration",
     ]
 
     resources = [
@@ -49,9 +55,16 @@ data "aws_iam_policy_document" "ssmpatchiam_policy" {
   }
 }
 
+resource "aws_iam_policy" "patchS3Bucket_policy" {
+  name        = "${local.default_tag}-s3patchbucket-policy"
+  description = "This is the inline policy for the s3 bucket used to log patch manager"
+  policy      = data.aws_iam_policy_document.ssmpatchiam_policy2.json
+}
 
 resource "aws_iam_role" "ssmpatch_role" {
-  name               = "${local.default_tag}-ssmpatchrole"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.ssmpatchiam_policy.json
+  name                = "${local.default_tag}-ssmpatchrole"
+  description         = "This is the role used for SSM patch instance profile"
+  path                = "/"
+  assume_role_policy  = data.aws_iam_policy_document.ssmpatchiam_policy.json
+  managed_policy_arns = [aws_iam_policy.patchS3Bucket_policy.arn]
 }
