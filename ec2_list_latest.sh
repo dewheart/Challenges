8b2d0cf86c5c03f121dcda76bda968c9f7964818
dewheart <44681331+dewheart@users.noreply.github.com>
2025-05-05 14:57:12 -0400

    Add files via upload

--- ec2_list_latest.sh
+++ ec2_list_latest.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Role name in member accounts you can assume
 ROLE_NAME="OrganizationAccountAccessRole"
 
-# Get list of account IDs (requires access to organizations:ListAccounts)
+# Get management (root) account ID
+MGMT_ACCOUNT_ID=$(aws organizations describe-organization \
+  --query "Organization.MasterAccountId" --output text)
+
+# Get list of all ACTIVE account IDs, excluding the management account
 ACCOUNT_IDS=$(aws organizations list-accounts \
-  --query "Accounts[?Status=='ACTIVE'].Id" --output text)
+  --query "Accounts[?Status=='ACTIVE' && Id!='${MGMT_ACCOUNT_ID}'].Id" \
+  --output text)
 
 for ACCOUNT_ID in $ACCOUNT_IDS; do
   echo "Getting EC2 data for account: $ACCOUNT_ID"
@@ -22,6 +26,5 @@ for ACCOUNT_ID in $ACCOUNT_IDS; do
     --query "Reservations[*].Instances[*].{AccountId:\"$ACCOUNT_ID\", InstanceId:InstanceId, ImageId:ImageId}" \
     --output table
 
-  # Optional: Unset temp credentials
   unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
 done
